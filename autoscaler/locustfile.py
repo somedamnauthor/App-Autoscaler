9d8f273fbcaa71aaf83e6846c74a5c3f861f44af
somedamnauthor <sreeshnelp@gmail.com>
2022-04-25 20:07:59 +0200

    Added locust file, added experiment results

---
+++ autoscaler/locustfile.py
@@ -0,0 +1,13 @@
+from random import randrange
+from locust import HttpUser, task
+
+class User(HttpUser):
+
+    @task
+    def getData(self):
+        self.client.get("/")
+
+    @task
+    def putData(self):
+        objIdString = "/objs/" + str(randrange(10000))
+        self.client.put(objIdString, {"content":"swarm attack2!"})
