9ba8513bb5da9c674ec77dc568f01293ffd480c6
somedamnauthor <sreeshnelp@gmail.com>
2022-04-27 19:42:53 +0200

    Added scaling down functionality to autoscaler

--- autoscaler/autoscaler.py
+++ autoscaler/autoscaler.py
@@ -7,38 +7,8 @@ import requests
 
 import time
 
-# from podman import PodmanClient
 import os
 
-def monitor():
-
-    CSV_URL = "http://localhost:9999/stats;csv"
-
-    numRequests = 0
-
-    while True:
-        
-        with requests.Session() as s:
-            download = s.get(CSV_URL)
-
-        decoded_content = download.content.decode('utf-8')
-        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-        dataList = list(cr)
-
-        #The first value in the list tells us what type of resource is being monitored by haproxy
-        #Thus the number of entries in the list of rows that have the value 'app' in the 0th index tells us how many containers we have running
-        #The above value is 1 more than the actual number of containers, so 1 is subtracted
-        numContainers = [row[0] for row in dataList].count('app') - 1
-
-        #dataList[-1] corresponds to the 'backend' row in the stats page. This row has the stats for all the backends put together
-        #The [7]th index contains the total number of requests that have been made up until that point
-        #Thus the 'change' in the number of requests between now and the previous instant is calculated
-        newRequests = int(dataList[-1][7]) - numRequests
-        numRequests = int(dataList[-1][7])
-        
-        print(newRequests)
-        
-        time.sleep(1)
 
 
 def spawnContainers(numContainersPresent, numContainersToSpawn):
@@ -67,15 +37,125 @@ def spawnContainers(numContainersPresent, numContainersToSpawn):
 
 
 def removeContainers(numContainersToDelete):
+
+    if numContainersToDelete == 0:
+        return
+
+    filepath = '../haproxyFiles/haproxy/haproxy.cfg'
+
+    newFileList = []
+    skippedContainers = []
     
-    # for line in reversed(list(open('../haproxyFiles/haproxy/haproxy.cfg'))):
-    #     print(line.rstrip())
+    #Create a new file without the required number of backends, so that the containers are removed from the haproxy config
+    numLinesSkipped = 0
+    for line in reversed(list(open(filepath))):
 
-    with 
+        if numLinesSkipped < numContainersToDelete and line.rstrip()!='':
+            
+            numLinesSkipped += 1
+
+            left = 'server  '
+            right = ' mycontainer'
+            skippedContainers.append(line[line.index(left)+len(left):line.index(right)])
+            continue
+
+        else:
+
+            newFileList.append(line.rstrip())
+    
+    newFileList.reverse()
+
+    while newFileList[-1]=='':
+        newFileList.pop(-1)
+
+    #Write the contents onto file
+    with open(filepath, 'w') as f:
+        for item in newFileList:
+            f.write("%s\n" % item)
+
+    #Kill HAProxy container, which is set to restart with the new config
+    os.system('podman kill -s HUP lbcontainer')
+
+    #Kill the containers
+    for app in skippedContainers:
+        if app[-1]!='1':
+            containerName = 'mycontainer'+app[-1]
+            # deleteCommand = 'podman rm -f '+containerName
+            deleteCommand = 'podman kill '+containerName
+            print(deleteCommand)
+            os.system(deleteCommand)
 
 
 
-# monitor()
+def monitor():
 
-# spawnContainers(1, 2)
-removeContainers(4,2)
+    CSV_URL = "http://localhost:9999/stats;csv"
+
+    numRequests = 0
+
+    justScaled = False
+    firstTryAction = True
+
+    while True:
+        
+        with requests.Session() as s:
+            download = s.get(CSV_URL)
+
+        decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        dataList = list(cr)
+
+        #The first value in the list tells us what type of resource is being monitored by haproxy
+        #Thus the number of entries in the list of rows that have the value 'app' in the 0th index tells us how many containers we have running
+        #The above value is 1 more than the actual number of containers, so 1 is subtracted
+        numContainers = [row[0] for row in dataList].count('app') - 1
+
+        #dataList[-1] corresponds to the 'backend' row in the stats page. This row has the stats for all the backends put together
+        #The [7]th index contains the total number of requests that have been made up until that point
+        #Thus the 'change' in the number of requests between now and the previous instant is calculated
+        
+        initialRequests = numRequests
+        newRequests = int(dataList[-1][7]) - numRequests
+        numRequests = int(dataList[-1][7])
+
+        if justScaled == True:
+            print("\nLetting requests update\n")
+            justScaled = False
+            continue
+        
+        if initialRequests!=0 and newRequests>0:
+
+            if firstTryAction == True:
+                print("\nDelaying action to next try\n")
+                firstTryAction = False
+                continue
+
+            if numContainers*20 <= newRequests:
+                numToSpawn = int(newRequests/20) - numContainers + 1
+
+                print("\nNew requests:", newRequests, "\nCurrent containers: ", numContainers, "\nNumber of containers to spawn: ",numToSpawn)
+                spawnContainers(numContainers, numToSpawn)
+                justScaled = True
+                firstTryAction = True
+                time.sleep(1)
+
+            else:
+                numToDelete = numContainers - int(newRequests/20) - 1
+                print("\nNew requests:", newRequests, "\nCurrent containers: ", numContainers, "\nNumber of containers to delete: ",numToDelete)
+                if numToDelete > 0:
+                    # removeContainers(numToDelete)
+                    removeContainers(1)
+                    justScaled = True
+                    firstTryAction = True
+                    time.sleep(1)
+                else:    
+                    time.sleep(1)
+
+        else:
+            print("No new requests")
+            time.sleep(1)
+
+# removeContainers(3)
+# spawnContainers(1, 4)
+
+monitor()
